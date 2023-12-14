d5da2d155b9083c7db84e0d74c4033bacf164c89
nickwild77 <knezhevich77@gmail.com>
2023-12-14 22:44:33 +0300

    CI + dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,18 @@ import logging
 import sys
 import pytest
 
+
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler(sys.stdout)
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
+
+
+@pytest.fixture()
+def processed_dataset_path():
+    curdir = os.path.dirname(__file__)
+    logger.info(curdir)
+    return os.path.join(curdir, "sampled_preprocessed_train_50k.csv")
 
 
 @pytest.fixture()
