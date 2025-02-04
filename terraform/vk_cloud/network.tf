c57d6c89c7588d43f8ab537ac2319a831630340b
nickwild77 <knezhevich77@gmail.com>
2025-02-04 13:14:02 +0300

    jenkinsfile added1

---
+++ terraform/vk_cloud/network.tf
@@ -0,0 +1,26 @@
+data "vkcs_networking_network" "extnet" {
+  name = "internet"
+}
+
+resource "vkcs_networking_network" "k8s" {
+  name           = "k8s-net"
+  admin_state_up = true
+}
+
+resource "vkcs_networking_subnet" "k8s" {
+  name            = "k8s-subnet"
+  network_id      = vkcs_networking_network.k8s.id
+  cidr            = "192.168.199.0/24"
+  dns_nameservers = ["8.8.8.8", "8.8.4.4"]
+}
+
+resource "vkcs_networking_router" "k8s" {
+  name                = "k8s-router"
+  admin_state_up      = true
+  external_network_id = data.vkcs_networking_network.extnet.id
+}
+
+resource "vkcs_networking_router_interface" "k8s" {
+  router_id = vkcs_networking_router.k8s.id
+  subnet_id = vkcs_networking_subnet.k8s.id
+}
