0619bb07e81928df5d97af53869862161c6583c7
nickwild77 <knezhevich77@gmail.com>
2025-02-20 20:58:14 +0300

    s3 download dag for airflow

--- dags/s3_download.py
+++ dags/s3_download.py
@@ -1,16 +1,17 @@
-from boto_examples import download_data
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+
+from boto_examples import download_data
 
 with DAG(
         dag_id="download_data_from_s3",
         start_date=airflow.utils.dates.days_ago(5),
         schedule_interval="@weekly",
 ) as dag:
-    task_download_from_s3 = PythonOperator(
+    download_from_s3 = PythonOperator(
         task_id='Data download from s3',
         python_callable=download_data,
         dag=dag
     )
 
-    task_download_from_s3
+    download_from_s3
